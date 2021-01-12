bdab1fb0b83cda032b93d095dde6ec28b1a89420
nkalpaxis <58003892+nkalpaxis@users.noreply.github.com>
2021-01-12 12:37:18 -0500

    Add files via upload

--- numberfirescrape.py
+++ numberfirescrape.py
@@ -1,32 +1,54 @@
 from bs4 import BeautifulSoup
+
 import requests
+
 import csv 
 
-response = requests.get('https://www.numberfire.com/nba/daily-fantasy/daily-basketball-projections')
 
-status = response.status_code
-
-content = response.content
-
-parser = BeautifulSoup(content, 'html.parser')
 
 player_names = []
+
 player_projections = []
 
+
 def scrape_projections(url):
-       
+
+
+
         response = requests.get(url)
+
         content = response.content
+
         parser = BeautifulSoup(content, 'html.parser')
+
         for p_name in parser.find_all('a',class_= 'full'):
-            player_name = p_name.text.strip() 
+
+            player_name = p_name.text.strip()
+
             player_names.append(player_name)
+
         for p_proj in parser.find_all('td', class_='fp active'):
-            player_proj = p_proj.text.strip().rstrip()
+
+            player_proj = p_proj.text.strip()
+
             player_projections.append(player_proj)
-        with open('projections.csv', 'w') as csv_file:
+
+        if len(player_names) != len(player_projections):
+
+            print(f'number of player names ({len(player_names)}) does not equal ' +
+
+                  f'number of player projections ({len(player_projections)})')
+
+            return
+
+        with open('projections.csv', 'w', newline='') as csv_file:
+
             writer = csv.writer(csv_file)
-            writer.writerows(im a bot)
+
+            for i in range(len(player_names)):
+
+                writer.writerow([player_names[i], player_projections[i]])
+
 
 
 scrape_projections('https://www.numberfire.com/nba/daily-fantasy/daily-basketball-projections')
